0828c8980c5bf69efe81f3f7225f96712a23cb90
kratess <parvudoruionut@yahoo.ro>
2022-06-23 21:43:21 +0000

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import background from "./background.png";
+import characters from "./characters.png";
+import girl from "./girl.png";
+import logo from "./logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full fixed" style={{
+      background: `url('${background}') center`,
+      backgroundSize: "cover"
+    }}>
+      <div className='absolute z-10' style={{
+        width: 128,
+        top: 32,
+        left: 32,
+        filter: "drop-shadow(2px 4px 6px #fb7600)"
+      }}>
+        <img src={logo} alt="" />
+      </div>
+
+      <div className="w-full h-full absolute" style={{
+        filter: "drop-shadow(#FB7600 5px 5px 15px)"
+      }}>
+        <div className="w-9/12 h-3/6 absolute" style={{
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%) skew(-10deg, -1deg)",
+          background: "linear-gradient(110deg, rgba(210,163,120,1) 2%, rgba(246,127,21,1) 10%, rgba(251,118,0,1) 85%, rgba(210,184,160,1) 95%)",
+          //clipPath: "polygon(20% 25%, 95% 20%, 85% 60%, 10% 65%)"
+        }}>
+          <div style={{padding: "32px 64px"}}>
+            <h2 className='font-bold'>Server Innovativo</h2>
+            <h3 className='font-bold'>Nuove meccaniche, nuovi script, nuove macchine</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className='absolute' style={{
+        bottom: 0,
+        width: 632
+      }}>
+        <img src={girl} alt="" />
+      </div>
+
+      <div className='absolute' style={{
+        bottom: 0,
+        right: "10%",
+        width: 600
+      }}>
+        <img src={characters} alt="" />
+      </div>
     </div>
   );
 }
